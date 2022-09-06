17fa167bd7454a89c64922bd2a6369d64607caaf
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-09-06 11:23:51 +0200

    Passing test

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -13,14 +13,11 @@ defmodule FizzBuzz do
 
   """
   def convert(number) do
-	if rem(number, 3) == 0 do
-		"Fizz"
-  	else
-		if rem(number, 5) == 0 do
-			"Buzz"
-		else
-			Integer.to_string(number) 
-		end
+	cond do
+		rem(number, 3) == 0 and rem(number, 5) == 0 -> "FizzBuzz"
+		rem(number, 3) == 0 -> "Fizz"
+		rem(number, 5) == 0 -> "Buzz"
+		true -> Integer.to_string(number) 
   	end
   end
 end
